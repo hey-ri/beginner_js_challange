c497751c3dd0059f62bd7dab9de917af869042f7
hye-ri <kwonhr01@naver.com>
2022-09-06 18:44:32 +0900

    수정

--- 4.0.form/js/script.js
+++ 4.0.form/js/script.js
@@ -8,19 +8,19 @@ const USERNAME_KEY = "username";
 function onLoginSubmit(e) {
     e.preventDefault(); /* submit기능은 저절로 보내는 기능이 생겨서 막아주기 */
     //console.log(loginInput.value);
-    const username = loginInput.value;
-    /* localStorage는 미니DB같은 존재다. 개발자도구 애플리케이션의 로컬스토리지에 들어가면 볼 수 있는데 key,value값이 필요로 된다. */
-    localStorage.setItem(USERNAME_KEY, username);
-
     loginForm.classList.add(HIDDEN_CLASSNAME);
+    const wroteUsername = loginInput.value;
+    /* localStorage는 미니DB같은 존재다. 개발자도구 애플리케이션의 로컬스토리지에 들어가면 볼 수 있는데 key,value값이 필요로 된다. */
+    localStorage.setItem(USERNAME_KEY, wroteUsername);
     //console.log(userName);
-    paintingGreeting(username);
+    paintingGreeting();
 }
 
 function paintingGreeting(username) {
     greeting.classList.remove(HIDDEN_CLASSNAME);
     greeting.innerText = `Hello ${username}`;
-    /* 인자 usename은 상황에 따라 바뀔 수 있음 그래서 hello + username으로 해줌 밑에 식에서는 save어쩌고로 쓰이고 위에서는 username으로 쓰임 */
+    /* 인자 usename은 상황에 따라 바뀔 수 있음 그래서 hello + username으로 해줌 밑에 식에서는 save어쩌고로 쓰이고 위에서는 username으로 쓰임 
+    !localStorage에 유저이름이 저장 돼 있어서 굳이 인자를 줄 필요는 없음! */
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
@@ -32,5 +32,5 @@ if (savedUsername === null) {
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
     //show greetings
-    paintingGreeting(savedUsername);
+    paintingGreeting();
 }
