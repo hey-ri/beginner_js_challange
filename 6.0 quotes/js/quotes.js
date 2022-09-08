32093644945b38462b7c631544a6e12ad88edc67
hye-ri <kwonhr01@naver.com>
2022-09-08 18:42:59 +0900

    배경 랜덤으로 추가하기

--- 6.0 quotes/js/quotes.js
+++ 6.0 quotes/js/quotes.js
@@ -43,6 +43,8 @@ const quotes = [
 
 const quote = document.querySelector("#quote span:first-child");
 const author = document.querySelector("#quote span:last-child");
+//console.log(quotes[Math.floor(Math.random() * quotes.length)]);
+
 const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
 quote.innerText = todaysQuote.quote;
