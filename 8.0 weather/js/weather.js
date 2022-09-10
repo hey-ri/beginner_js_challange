802d4a560cebc1c236743c0802f269fb49ed8830
hye-ri <kwonhr01@naver.com>
2022-09-10 22:15:05 +0900

    weather추가 /API사용

---
+++ 8.0 weather/js/weather.js
@@ -0,0 +1,22 @@
+const API_KEY = "4eee2d7be008bc924e4a2dbef5f00a61";
+
+function onGeoOk(position) {
+    //console.log(position);
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    console.log("You live it", lat, lon);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    //console.log(url);
+    fetch(url) //fetch는 promise기다리는 함수라 then을 이용해서 바로 나타내주기
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        });
+}
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
+}
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
