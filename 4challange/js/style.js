1cf3634bc526d5327e1ab16350b74484289dc589
hye-ri <kwonhr01@naver.com>
2022-09-15 22:43:46 +0900

    document.ready는 jquery 사용문,,

--- 4challange/js/style.js
+++ 4challange/js/style.js
@@ -1,31 +1,29 @@
-$(function () {
-    const form = document.querySelector("#form");
-    const inputNum = document.querySelector("input");
-    const guessInput = document.querySelector(".guess input");
-    const p = document.querySelector("p");
-    const strong = document.querySelector("strong");
+const form = document.querySelector("#form");
+const inputNum = document.querySelector("input");
+const guessInput = document.querySelector(".guess input");
+const p = document.querySelector("p");
+const strong = document.querySelector("strong");
 
-    const HIDDEN = "hidden";
+const HIDDEN = "hidden";
 
-    function onSubmit(e) {
-        e.preventDefault();
-        let random = Math.floor(Math.random() * inputNum.value) + 1;
-        //console.log(random);
-        let guessNum = guessInput.value;
-        const num = parseInt(guessNum, 10);
+function onSubmit(e) {
+    e.preventDefault();
+    let random = Math.floor(Math.random() * inputNum.value) + 1;
+    //console.log(random);
+    let guessNum = guessInput.value;
+    const num = parseInt(guessNum, 10);
 
-        p.innerText = `You chose ${guessNum} , the machine chose ${random}`;
+    p.innerText = `You chose ${guessNum} , the machine chose ${random}`;
 
-        if (num === random) {
-            p.classList.remove(HIDDEN);
-            strong.innerText = "You Win";
-            strong.classList.remove(HIDDEN);
-        } else {
-            p.classList.remove(HIDDEN);
-            strong.innerText = "You lose";
-            strong.classList.remove(HIDDEN);
-        }
+    if (num === random) {
+        p.classList.remove(HIDDEN);
+        strong.innerText = "You Win";
+        strong.classList.remove(HIDDEN);
+    } else {
+        p.classList.remove(HIDDEN);
+        strong.innerText = "You lose";
+        strong.classList.remove(HIDDEN);
     }
+}
 
-    form.addEventListener("submit", onSubmit);
-});
+form.addEventListener("submit", onSubmit);
